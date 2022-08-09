c549a2cd0467666a9f30ea78916a3b1add96b346
Roop Sai Krishna <roopsai@rudderstack.com>
2022-08-09 11:58:23 +0530

    exp: python gha fix

--- scripts/test.py
+++ scripts/test.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
-import requests
-import json
+#import requests
+#import json
 import os
-import sys
-import jsondiff
+#import sys
+#import jsondiff
 
 print("Hello world")
 
 #########################
 # ENV VARIABLES
-CONTROL_PLANE_URL=sys.argv[1]
-print(CONTROL_PLANE_URL)
+#CONTROL_PLANE_URL=sys.argv[1]
+#print(CONTROL_PLANE_URL)
 USERNAME=os.environ['API_USER'] #sys.argv[2]
 print(USERNAME)
 PASSWORD=os.environ['API_PASSWORD'] #sys.argv[3]
 #print(PASSWORD)
 #########################
 # CONSTANTS
-HEADER = {"Content-Type": "application/json"}
-AUTH = (USERNAME, PASSWORD)
+#HEADER = {"Content-Type": "application/json"}
+#AUTH = (USERNAME, PASSWORD)
 #########################
