2d23994ac122cb579bcd7dc2a328a7860a88568e
Roop Sai Krishna <roopsai@rudderstack.com>
2022-08-09 12:04:37 +0530

    fix: argv in comments

--- scripts/test.py
+++ scripts/test.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
-#import requests
-#import json
+
+import json
+import jsondiff
 import os
-#import sys
-#import jsondiff
+import requests
+import sys
 
 print("Hello world")
 
 #########################
 # ENV VARIABLES
-#CONTROL_PLANE_URL=sys.argv[1]
-#print(CONTROL_PLANE_URL)
 USERNAME=os.environ['API_USER'] #sys.argv[2]
 print(USERNAME)
 PASSWORD=os.environ['API_PASSWORD'] #sys.argv[3]
