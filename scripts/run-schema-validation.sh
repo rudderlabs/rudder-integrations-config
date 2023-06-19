7aa24d1aa2986db70d7215bdf1f5451186c46eeb
Akshat mangal <64827980+iamakshat01@users.noreply.github.com>
2023-06-19 10:25:05 +0530

    chore(ci): add test for schema validation (#682)

---
+++ scripts/run-schema-validation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+changed_files="$1"
+exit_code=0
+last_directory=""
+for file in $changed_files; do
+    directory=$(dirname "${file}")
+    parent_directory=$(dirname "${directory}")
+    if [[ "$parent_directory" == "src/configurations/destinations" || "$parent_directory" == "src/configurations/sources" ]]; then
+        name=$(echo "$file" | cut -d '/' -f 4)
+        selector=$(echo "$parent_directory" | cut -d '/' -f 3)
+        selector=${selector::-1}
+        # Storing the last directory to ensure that validation runs only once, even if multiple configuration files are changed for a given source or destination.
+        if [ "$last_directory" != "${directory}" ]; then
+            output==$(python scripts/schemaGenerator.py -name="$name" $selector 2>&1)
+            warnings=$(echo "$output" | grep -i "warning" || true)
+            recommendations=$(echo "$output" | grep -i "recommendation" || true)
+            if [ -n "$warnings" ]; then
+                echo "Warnings found for name: ${name} selector: ${selector}:"
+                echo "$warnings"
+                exit_code=1
+            fi
+            if [ -n "$recommendations" ]; then
+                echo "For name: ${name} selector: ${selector}: $recommendations"
+            fi
+        fi
+        last_directory=${directory}
+    fi
+done
+exit $exit_code
