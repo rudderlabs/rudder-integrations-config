2fa597f67f72968a8e24ba7fac17ec88f998cbb1
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-07-17 17:15:09 +0530

    fix: refactor code and add unit test

--- scripts/configGenerator.py
+++ scripts/configGenerator.py
@@ -1,82 +1,86 @@
 import json
 import os
 
-# Open the JSON file
-with open('scripts/data.json', 'r') as file:
-    # Load the JSON data
-    data = json.load(file)
+def generateConfigs(data):
+    # Read the content of template-db-config.json
+    with open('scripts/template-db-config.json', 'r') as file:
+        template_db_config = json.load(file)
 
-# Read the content of template-db-config.json
-with open('scripts/template-db-config.json', 'r') as file:
-    template_db_config = json.load(file)
+    # Read the content of template-ui-config.json
+    with open('scripts/template-ui-config.json', 'r') as file:
+        template_ui_config = json.load(file)
 
-# Read the content of template-ui-config.json
-with open('scripts/template-ui-config.json', 'r') as file:
-    template_ui_config = json.load(file)
+    # Create db-config object with the same content
+    db_config = template_db_config
 
-# Create db-config object with the same content
-db_config = template_db_config
+    db_config['displayName'] = data['displayName']
+    formFields = data['formFields']
 
-db_config['displayName'] = data['displayName']
-formFields = data['formFields']
+    # Create db-config object with the same content
+    ui_config = template_ui_config
 
-# Create db-config object with the same content
-ui_config = template_ui_config
+    # Access the necessary location and update the fields array
 
-# Access the necessary location and update the fields array
-
-def appendFieldsInGroups(settings, field):
-    if "sections" in settings and settings["sections"]:
-        groups = settings["sections"][0]["groups"]
-        if groups:
-            first_group = groups[0]
-            if "fields" in first_group:
-                first_group["fields"].append(field)
+    def appendFieldsInGroups(settings, field):
+        if "sections" in settings and settings["sections"]:
+            groups = settings["sections"][0]["groups"]
+            if groups:
+                first_group = groups[0]
+                if "fields" in first_group:
+                    first_group["fields"].append(field)
 
 
-def updateUiConfig(field):
-    if "uiConfig" in ui_config and "baseTemplate" in ui_config["uiConfig"]:
-        base_template = ui_config["uiConfig"]["baseTemplate"]
-        if base_template and field['required'] == True:
-            connection_settings = base_template[0]
-            appendFieldsInGroups(connection_settings, field)
-        elif base_template:
-            configuration_settings = base_template[1]
-            appendFieldsInGroups(configuration_settings, field)
-        return ui_config
+    def updateUiConfig(field):
+        if "uiConfig" in ui_config and "baseTemplate" in ui_config["uiConfig"]:
+            base_template = ui_config["uiConfig"]["baseTemplate"]
+            if base_template and field['required'] == True:
+                connection_settings = base_template[0]
+                appendFieldsInGroups(connection_settings, field)
+            elif base_template:
+                configuration_settings = base_template[1]
+                appendFieldsInGroups(configuration_settings, field)
+            return ui_config
 
 
-# Iterate over JSON objects in the array
-for obj in formFields:
-    # update field in ui-config
-    ui_config = updateUiConfig(obj)
-    # update db-config
-    for key, value in obj.items():
-        if key == 'configKey':
-            db_config['config']['destConfig']['defaultConfig'].append(
-                value)
-        if key == 'secret' and value == True:
-            db_config['config']['secretKeys'].append(
-                db_config['config']['destConfig']['defaultConfig'][-1])
+    # Iterate over JSON objects in the array
+    for obj in formFields:
+        # update field in ui-config
+        ui_config = updateUiConfig(obj)
+        # update db-config
+        for key, value in obj.items():
+            if key == 'configKey':
+                db_config['config']['destConfig']['defaultConfig'].append(
+                    value)
+            if key == 'secret' and value == True:
+                db_config['config']['secretKeys'].append(
+                    db_config['config']['destConfig']['defaultConfig'][-1])
+
+    db_config = json.dumps(db_config)
+    return [db_config, ui_config]
 
 
-file_path = f'src/configurations/destinations/{data["displayName"]}/db-config.json'
-db_config = json.dumps(db_config)
-directory = os.path.dirname(file_path)
-if not os.path.exists(directory):
-    os.makedirs(directory)
+if __name__ == '__main__':
+    with open('scripts/data.json', 'r') as file:
+        # Load the JSON data
+        data = json.load(file)
 
-with open(file_path, 'w') as file:
-    # Write the new content
-    file.write(db_config)
+    configData = generateConfigs(data)
+    file_path = f'src/configurations/destinations/{data["displayName"]}/db-config.json'
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(file_path, 'w') as file:
+        # Write the new content
+        file.write(configData['db_config'])
 
 
-file_path = f'src/configurations/destinations/{data["displayName"]}/ui-config.json'
-ui_config = json.dumps(ui_config)
-directory = os.path.dirname(file_path)
-if not os.path.exists(directory):
-    os.makedirs(directory)
+    file_path = f'src/configurations/destinations/{data["displayName"]}/ui-config.json'
+    ui_config = json.dumps(configData['ui_config'])
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-with open(file_path, 'w') as file:
-    # Write the new content
-    file.write(ui_config)
+    with open(file_path, 'w') as file:
+        # Write the new content
+        file.write(ui_config)
