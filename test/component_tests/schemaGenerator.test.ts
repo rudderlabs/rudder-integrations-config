9efdb1adc7a801ac65564fed939cb944f8752780
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-02-07 15:39:47 +0530

    test: add test for legacy ui

--- test/component_tests/schemaGenerator.test.ts
+++ test/component_tests/schemaGenerator.test.ts
@@ -20,24 +20,37 @@ function readSchemaFile(filePath): string | undefined {
 describe('Schema Generator', () => {
   const configDir = 'test/component_tests/configurations';
 
-  it('should generate and save schema for the specified destination', () => {
-    const newSchemaData = readSchemaFile(
-      path.resolve(__dirname, './data/createNewSchemaDest.json'),
-    );
+  describe('should generate and save schema for the specified destination', () => {
+    const testData = [
+      {
+        description: 'New UI',
+        destName: 'create_new_schema_dest',
+        expectedSchemaFile: 'createNewSchemaDest.json',
+      },
+      {
+        description: 'Legacy UI',
+        destName: 'create_new_schema_dest_legacy_ui',
+        expectedSchemaFile: 'createNewSchemaDestLegacyUI.json',
+      },
+    ];
 
-    const destName = 'create_new_schema_dest';
-    const cmd = `CONFIG_DIR=${configDir} npm run update:schema:destination "${destName}"`;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    it.each(testData)('$description', ({ description, destName, expectedSchemaFile }) => {
+      const cmd = `CONFIG_DIR=${configDir} npm run update:schema:destination "${destName}"`;
 
-    execSync(cmd);
+      execSync(cmd);
 
-    const schemaFilePath = path.resolve(`${configDir}/destinations/${destName}/schema.json`);
+      const schemaFilePath = path.resolve(`${configDir}/destinations/${destName}/schema.json`);
 
-    const schema = readSchemaFile(schemaFilePath);
-    if (schema) {
-      // delete the file
-      fs.unlinkSync(schemaFilePath);
-    }
+      const schema = readSchemaFile(schemaFilePath);
+      if (schema) {
+        // delete the file
+        fs.unlinkSync(schemaFilePath);
+      }
 
-    expect(schema).toEqual(newSchemaData);
+      const newSchemaData = readSchemaFile(path.resolve(__dirname, `./data/${expectedSchemaFile}`));
+
+      expect(schema).toEqual(newSchemaData);
+    });
   });
 });
