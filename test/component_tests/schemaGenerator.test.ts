36f7da7ec4a9b954bb24b4c2c4f8ec7eab819614
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-02-07 16:27:43 +0530

    test: add more test scenarios for schema generator test suite

--- test/component_tests/schemaGenerator.test.ts
+++ test/component_tests/schemaGenerator.test.ts
@@ -17,6 +17,10 @@ function readSchemaFile(filePath): string | undefined {
   return schema;
 }
 
+function writeSchemaFile(filePath, schema) {
+  fs.writeFileSync(filePath, JSON.stringify(schema, null, 2));
+}
+
 describe('Schema Generator', () => {
   const configDir = 'test/component_tests/configurations';
 
@@ -48,9 +52,100 @@ describe('Schema Generator', () => {
         fs.unlinkSync(schemaFilePath);
       }
 
-      const newSchemaData = readSchemaFile(path.resolve(__dirname, `./data/${expectedSchemaFile}`));
+      const expectedSchemaData = readSchemaFile(
+        path.resolve(__dirname, `./data/${expectedSchemaFile}`),
+      );
 
-      expect(schema).toEqual(newSchemaData);
+      expect(schema).toEqual(expectedSchemaData);
+    });
+  });
+
+  describe('should not generate and save schema if update option is not provided for the specified destination', () => {
+    const testData = [
+      {
+        description: 'New UI',
+        destName: 'create_new_schema_dest',
+      },
+      {
+        description: 'Legacy UI',
+        destName: 'create_new_schema_dest_legacy_ui',
+      },
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    it.each(testData)('$description', ({ description, destName }) => {
+      const cmd = `CONFIG_DIR=${configDir} npm run check:schema:destination "${destName}"`;
+
+      execSync(cmd);
+
+      const schemaFilePath = path.resolve(`${configDir}/destinations/${destName}/schema.json`);
+
+      const schema = readSchemaFile(schemaFilePath);
+      expect(schema).toBeUndefined();
+    });
+  });
+
+  describe('should update and save schema for the specified destination', () => {
+    const testData = [
+      {
+        description: 'New UI',
+        destName: 'update_schema_dest',
+        expectedSchemaFile: 'updateSchemaDest.json',
+      },
+      {
+        description: 'Legacy UI',
+        destName: 'update_schema_dest_legacy_ui',
+        expectedSchemaFile: 'updateSchemaDestLegacyUI.json',
+      },
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    it.each(testData)('$description', ({ description, destName, expectedSchemaFile }) => {
+      const schemaFilePath = path.resolve(`${configDir}/destinations/${destName}/schema.json`);
+      const curSchema = readSchemaFile(schemaFilePath);
+
+      const cmd = `CONFIG_DIR=${configDir} npm run update:schema:destination "${destName}"`;
+
+      execSync(cmd);
+
+      const schema = readSchemaFile(schemaFilePath);
+      // Restore schema file
+      if (schema) {
+        writeSchemaFile(schemaFilePath, curSchema);
+      }
+
+      const expectedSchemaData = readSchemaFile(
+        path.resolve(__dirname, `./data/${expectedSchemaFile}`),
+      );
+
+      expect(schema).toEqual(expectedSchemaData);
+    });
+  });
+
+  describe('should not save the schema file if update option is not provided for the specified destination', () => {
+    const testData = [
+      {
+        description: 'New UI',
+        destName: 'update_schema_dest',
+      },
+      {
+        description: 'Legacy UI',
+        destName: 'update_schema_dest_legacy_ui',
+      },
+    ];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    it.each(testData)('$description', ({ description, destName }) => {
+      const schemaFilePath = path.resolve(`${configDir}/destinations/${destName}/schema.json`);
+      const curSchema = readSchemaFile(schemaFilePath);
+
+      const cmd = `CONFIG_DIR=${configDir} npm run check:schema:destination "${destName}"`;
+
+      execSync(cmd);
+
+      const schema = readSchemaFile(schemaFilePath);
+
+      expect(schema).toEqual(curSchema);
     });
   });
 });
