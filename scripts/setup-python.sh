c902b4a81a1a26854063260695041f46e34b5862
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-02-21 19:43:11 +0530

    chore: make python dependencies installation consistent

--- scripts/setup-python.sh
+++ scripts/setup-python.sh
@@ -2,7 +2,4 @@
 sudo apt-get update
 sudo apt-get -y install python3-pip
 pip3 --version
-pip3 install requests
-pip3 install jsonschema
-pip3 install jsondiff
-pip3 install black
+pip3 pip3 install -r ./requirements.txt
