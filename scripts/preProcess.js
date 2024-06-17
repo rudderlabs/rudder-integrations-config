391fe36dad9e093ac142f681717bd82d91ae94da
Utsab Chowdhury <utsab.c97@gmail.com>
2024-06-17 16:50:49 +0530

    chore: update for pre-process mechanism

--- scripts/preProcess.js
+++ scripts/preProcess.js
@@ -1,33 +1,29 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const { JsonTemplateEngine } = require('@rudderstack/json-template-engine');
 const fs = require('fs');
 const path = require('path');
 
 const srcPath = path.resolve(__dirname, '../src');
 
-function updateDefaults(jsonData, configKeyToUpdate, newDefaults) {
-  // Helper function for recursive traversal
-  function traverse(obj) {
-    if (typeof obj === 'object' && obj !== null) {
-      // Check for both objects and arrays
-      Object.keys(obj).forEach((key) => {
-        if (key === 'configKey' && obj[key] === configKeyToUpdate) {
-          // Found the object with the specified configKey
-          // eslint-disable-next-line dot-notation, no-param-reassign
-          obj['default'] = newDefaults;
-          return;
-        }
-        traverse(obj[key]);
-      });
-    }
-  }
+function getUiConfigTemplate(destinationName) {
+  const uiConfig = fs.readFileSync(
+    `${srcPath}/configurations/destinations/${destinationName}/ui-config.jt`,
+    'utf8',
+  );
+  return uiConfig;
+}
 
+function getUiDefaultData(destinationName) {
   try {
-    traverse(jsonData); // Start traversal from the root
+    const defaults = fs.readFileSync(
+      `${srcPath}/configurations/destinations/${destinationName}/ui-default.json`,
+      'utf8',
+    );
+    return JSON.parse(defaults);
   } catch (error) {
-    console.error(`Error while updating defaults for ${configKeyToUpdate}: ${error}`);
+    // skip the destination if ui-default.json is not present
+    return undefined;
   }
-
-  return jsonData;
 }
 
 function getDestinationNames() {
@@ -43,48 +39,22 @@ function getDestinationNames() {
   return destinationFolders;
 }
 
-function getUiConfigData(destinationName) {
-  const uiConfig = fs.readFileSync(
-    `${srcPath}/configurations/destinations/${destinationName}/ui-config.json`,
-    'utf8',
-  );
-  return JSON.parse(uiConfig);
-}
-
-function getUiDefaults(destinationName) {
-  try {
-    const defaults = fs.readFileSync(
-      `${srcPath}/configurations/destinations/${destinationName}/ui-default.json`,
-      'utf8',
-    );
-    return JSON.parse(defaults);
-  } catch (error) {
-    // skip the destination if ui-default.json is not present
-    return undefined;
-  }
-}
-
 function main() {
   const destinationFolders = getDestinationNames();
 
   destinationFolders.forEach((destinationName) => {
-    const uiDefaults = getUiDefaults(destinationName);
+    const uiDefaults = getUiDefaultData(destinationName);
     if (!uiDefaults) {
       return;
     }
-    const uiConfigData = getUiConfigData(destinationName);
 
-    // update the defaults in ui-config.json
-    Object.keys(uiDefaults).forEach((key) => {
-      const defaultVal = uiDefaults[key];
-      const updatedUIConfigData = updateDefaults(uiConfigData, key, defaultVal);
+    const uiConfigTemplate = getUiConfigTemplate(destinationName);
+    const result = JsonTemplateEngine.createAsSync(uiConfigTemplate).evaluate(uiDefaults);
 
-      console.log(`Updating defaults for ${key} in ${destinationName}`);
-      fs.writeFileSync(
-        `${srcPath}/configurations/destinations/${destinationName}/ui-config.json`,
-        JSON.stringify(updatedUIConfigData, null, 2),
-      );
-    });
+    fs.writeFileSync(
+      `${srcPath}/configurations/destinations/${destinationName}/ui-config.json`,
+      JSON.stringify(result, null, 2),
+    );
   });
 }
 
