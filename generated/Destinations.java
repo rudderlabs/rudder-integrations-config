5f589157b7c947ae7fd4ae46fe908588f8c67abf
Dilip Kola <33080863+koladilip@users.noreply.github.com>
2024-11-19 13:09:04 +0530

    feat: generate destination constants (#1800)

---
+++ generated/Destinations.java
@@ -0,0 +1,106 @@
+/*
+ GENERATED - DO NOT EDIT
+ This file is generated from the templates/Destinations.java.template file.
+ Orginal file: https://github.com/rudderlabs/rudder-integrations-config/blob/main/generated/Destinations.java
+
+ NOTE: Containly only constants for device mode destinations
+ */
+
+public class Destinations {
+  
+  public static final String ADJ_NAME = "ADJ";
+
+  public static final String ADJ_DISPLAY_NAME = "Adjust";
+
+  public static final String ADOBE_ANALYTICS_NAME = "ADOBE_ANALYTICS";
+
+  public static final String ADOBE_ANALYTICS_DISPLAY_NAME = "Adobe Analytics";
+
+  public static final String AF_NAME = "AF";
+
+  public static final String AF_DISPLAY_NAME = "AppsFlyer";
+
+  public static final String AM_NAME = "AM";
+
+  public static final String AM_DISPLAY_NAME = "Amplitude";
+
+  public static final String APPCENTER_NAME = "APPCENTER";
+
+  public static final String APPCENTER_DISPLAY_NAME = "App Center";
+
+  public static final String BRANCH_NAME = "BRANCH";
+
+  public static final String BRANCH_DISPLAY_NAME = "Branch Metrics";
+
+  public static final String BRAZE_NAME = "BRAZE";
+
+  public static final String BRAZE_DISPLAY_NAME = "Braze";
+
+  public static final String BUGSNAG_NAME = "BUGSNAG";
+
+  public static final String BUGSNAG_DISPLAY_NAME = "Bugsnag";
+
+  public static final String CLEVERTAP_NAME = "CLEVERTAP";
+
+  public static final String CLEVERTAP_DISPLAY_NAME = "CleverTap";
+
+  public static final String COMSCORE_NAME = "COMSCORE";
+
+  public static final String COMSCORE_DISPLAY_NAME = "Comscore";
+
+  public static final String CUSTOMERIO_NAME = "CUSTOMERIO";
+
+  public static final String CUSTOMERIO_DISPLAY_NAME = "Customer IO";
+
+  public static final String FB_NAME = "FB";
+
+  public static final String FB_DISPLAY_NAME = "Facebook App Events";
+
+  public static final String FIREBASE_NAME = "FIREBASE";
+
+  public static final String FIREBASE_DISPLAY_NAME = "Firebase";
+
+  public static final String FULLSTORY_NAME = "FULLSTORY";
+
+  public static final String FULLSTORY_DISPLAY_NAME = "Fullstory";
+
+  public static final String GA4_NAME = "GA4";
+
+  public static final String GA4_DISPLAY_NAME = "Google Analytics 4 (GA4)";
+
+  public static final String INTERCOM_NAME = "INTERCOM";
+
+  public static final String INTERCOM_DISPLAY_NAME = "Intercom";
+
+  public static final String KOCHAVA_NAME = "KOCHAVA";
+
+  public static final String KOCHAVA_DISPLAY_NAME = "Kochava";
+
+  public static final String LEANPLUM_NAME = "LEANPLUM";
+
+  public static final String LEANPLUM_DISPLAY_NAME = "Leanplum";
+
+  public static final String LOTAME_MOBILE_NAME = "LOTAME_MOBILE";
+
+  public static final String LOTAME_MOBILE_DISPLAY_NAME = "Lotame Mobile";
+
+  public static final String MOENGAGE_NAME = "MOENGAGE";
+
+  public static final String MOENGAGE_DISPLAY_NAME = "MoEngage";
+
+  public static final String OPTIMIZELY_FULLSTACK_NAME = "OPTIMIZELY_FULLSTACK";
+
+  public static final String OPTIMIZELY_FULLSTACK_DISPLAY_NAME = "Optimizely Fullstack";
+
+  public static final String QUALTRICS_NAME = "QUALTRICS";
+
+  public static final String QUALTRICS_DISPLAY_NAME = "Qualtrics";
+
+  public static final String SINGULAR_NAME = "SINGULAR";
+
+  public static final String SINGULAR_DISPLAY_NAME = "Singular";
+
+  public static final String SPRIG_NAME = "SPRIG";
+
+  public static final String SPRIG_DISPLAY_NAME = "Sprig";
+}
