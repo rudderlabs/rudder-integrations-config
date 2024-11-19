09deaaafecbc91c0675cb31a2414de5501e1cc75
Dilip Kola <33080863+koladilip@users.noreply.github.com>
2024-11-19 13:09:04 +0530

    feat: generate destination constants (#1800)

---
+++ generated/Destinations.swift
@@ -0,0 +1,106 @@
+/*
+ GENERATED - DO NOT EDIT
+ This file is generated from the templates/Destinations.swift.template file.
+ Orginal file: https://github.com/rudderlabs/rudder-integrations-config/blob/main/generated/Destinations.swift
+
+ NOTE: Containly only constants for device mode destinations
+ */
+
+struct Destinations {
+  
+  static let ADJ_NAME = "ADJ"
+
+  static let ADJ_DISPLAY_NAME = "Adjust"
+
+  static let ADOBE_ANALYTICS_NAME = "ADOBE_ANALYTICS"
+
+  static let ADOBE_ANALYTICS_DISPLAY_NAME = "Adobe Analytics"
+
+  static let AF_NAME = "AF"
+
+  static let AF_DISPLAY_NAME = "AppsFlyer"
+
+  static let AM_NAME = "AM"
+
+  static let AM_DISPLAY_NAME = "Amplitude"
+
+  static let APPCENTER_NAME = "APPCENTER"
+
+  static let APPCENTER_DISPLAY_NAME = "App Center"
+
+  static let BRANCH_NAME = "BRANCH"
+
+  static let BRANCH_DISPLAY_NAME = "Branch Metrics"
+
+  static let BRAZE_NAME = "BRAZE"
+
+  static let BRAZE_DISPLAY_NAME = "Braze"
+
+  static let BUGSNAG_NAME = "BUGSNAG"
+
+  static let BUGSNAG_DISPLAY_NAME = "Bugsnag"
+
+  static let CLEVERTAP_NAME = "CLEVERTAP"
+
+  static let CLEVERTAP_DISPLAY_NAME = "CleverTap"
+
+  static let COMSCORE_NAME = "COMSCORE"
+
+  static let COMSCORE_DISPLAY_NAME = "Comscore"
+
+  static let CUSTOMERIO_NAME = "CUSTOMERIO"
+
+  static let CUSTOMERIO_DISPLAY_NAME = "Customer IO"
+
+  static let FB_NAME = "FB"
+
+  static let FB_DISPLAY_NAME = "Facebook App Events"
+
+  static let FIREBASE_NAME = "FIREBASE"
+
+  static let FIREBASE_DISPLAY_NAME = "Firebase"
+
+  static let FULLSTORY_NAME = "FULLSTORY"
+
+  static let FULLSTORY_DISPLAY_NAME = "Fullstory"
+
+  static let GA4_NAME = "GA4"
+
+  static let GA4_DISPLAY_NAME = "Google Analytics 4 (GA4)"
+
+  static let INTERCOM_NAME = "INTERCOM"
+
+  static let INTERCOM_DISPLAY_NAME = "Intercom"
+
+  static let KOCHAVA_NAME = "KOCHAVA"
+
+  static let KOCHAVA_DISPLAY_NAME = "Kochava"
+
+  static let LEANPLUM_NAME = "LEANPLUM"
+
+  static let LEANPLUM_DISPLAY_NAME = "Leanplum"
+
+  static let LOTAME_MOBILE_NAME = "LOTAME_MOBILE"
+
+  static let LOTAME_MOBILE_DISPLAY_NAME = "Lotame Mobile"
+
+  static let MOENGAGE_NAME = "MOENGAGE"
+
+  static let MOENGAGE_DISPLAY_NAME = "MoEngage"
+
+  static let OPTIMIZELY_FULLSTACK_NAME = "OPTIMIZELY_FULLSTACK"
+
+  static let OPTIMIZELY_FULLSTACK_DISPLAY_NAME = "Optimizely Fullstack"
+
+  static let QUALTRICS_NAME = "QUALTRICS"
+
+  static let QUALTRICS_DISPLAY_NAME = "Qualtrics"
+
+  static let SINGULAR_NAME = "SINGULAR"
+
+  static let SINGULAR_DISPLAY_NAME = "Singular"
+
+  static let SPRIG_NAME = "SPRIG"
+
+  static let SPRIG_DISPLAY_NAME = "Sprig"
+}
