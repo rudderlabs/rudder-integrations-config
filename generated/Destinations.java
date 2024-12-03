34876ef5cb68b3e419b2578d6b698facc9463b11
Dilip Kola <33080863+koladilip@users.noreply.github.com>
2024-12-03 13:11:04 +0530

    chore: add generate constant script to pre commit (#1821)

--- generated/Destinations.java
+++ generated/Destinations.java
@@ -1,7 +1,7 @@
 /*
  GENERATED - DO NOT EDIT
  This file is generated from the templates/Destinations.java.template file.
- Orginal file: https://github.com/rudderlabs/rudder-integrations-config/blob/main/generated/Destinations.java
+ Orginal file: https://github.com/rudderlabs/rudder-integrations-config/blob/develop/generated/Destinations.java
 
  NOTE: Containly only constants for device mode destinations
  */
